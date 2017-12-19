41b7eef369c8099056d12d244193a8753e94c763
Xiaonan Shen <sxn@shenxn.com>
2017-12-19 11:24:38 +0800

    [Update] Add interval between fetches

--- light/honeywell_hgw2000.py
+++ light/honeywell_hgw2000.py
@@ -1,5 +1,6 @@
 import logging
 import voluptuous as vol
+import time
 
 from homeassistant.components.light import Light, PLATFORM_SCHEMA
 from homeassistant.const import CONF_HOST, CONF_DEVICES
@@ -9,8 +10,11 @@ DEPENDENCIES = ['honeywell_hgw2000']
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_INTERVAL = 'interval'
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
   vol.Optional(CONF_DEVICES, default=[]): vol.All(cv.ensure_list, [dict]),
+  vol.Optional(CONF_INTERVAL, default = 2): cv.positive_int,
 })
 
 HONEYWELL_HGW2000_API = 'honeywell_hgw2000_api'
@@ -18,16 +22,20 @@ HONEYWELL_HGW2000_API = 'honeywell_hgw2000_api'
 def setup_platform(hass, config, add_devices, discovery_info=None):
   host = config.get(CONF_HOST)
   lights = config.get(CONF_DEVICES)
+  interval = config.get(CONF_INTERVAL)
+  api = hass.data[HONEYWELL_HGW2000_API]
 
-  add_devices(HoneywellLight(hass.data[HONEYWELL_HGW2000_API], light) for light in lights)
+  total_interval = interval * len(lights)
+  add_devices(HoneywellLight(api, total_interval, lights[i], i * interval) for i in range(len(lights)))
 
 class HoneywellLight(Light):
-  def __init__(self, api, light):
-    self._api = api
+  def __init__(self, api, interval, light, initial_update):
     self._name = light['name']
     self._id = light['id']
     self._state = None
-    self.update()
+    self._last_update = time.time() + initial_update - interval
+    self._api = api
+    self._interval = interval
   
   @property
   def name(self):
@@ -41,6 +49,7 @@ class HoneywellLight(Light):
     if states['rt'] != '0':
       return False
     self._state = states[key] == '1'
+    self._last_update = time.time()
     return True
 
   def turn(self, switch):
@@ -48,13 +57,13 @@ class HoneywellLight(Light):
 
   def turn_on(self):
     if not self.turn(1):
-      return self.turn(1)
-    return True
+      self.turn(1)
   
   def turn_off(self):
     if not self.turn(0):
-      return self.turn(0)
-    return True
+      self.turn(0)
 
   def update(self):
-    return self.update_state(self._api.request('querylight', 'lightid={id}&_='.format(id = self._id)), 'switch')
+    if self._last_update == None or self._last_update + self._interval < time.time():
+      return self.update_state(self._api.request('querylight', 'lightid={id}&_='.format(id = self._id)), 'switch')
+    return True
